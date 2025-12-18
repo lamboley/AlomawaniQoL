d6d778ea9062dd69dc2f27077ecd944e3952b683
Lucas Lamboley <l.lamboley.mentor@aleo.agency>
2025-12-18 21:41:38 +0100

    :art: Optimize

--- AlomawaniQoL.lua
+++ AlomawaniQoL.lua
@@ -1,76 +1,75 @@
-local _G = _G
-local type= type
-
-local AceAddon, AceAddonMinor = _G.LibStub('AceAddon-3.0')
-
 local addonName, AlomawaniQoL = ...
+
+local AceAddon = LibStub('AceAddon-3.0')
+local LibDualSpec = LibStub('LibDualSpec-1.0', true)
+
 AlomawaniQoL = AceAddon:NewAddon(AlomawaniQoL, 'AlomawaniQoL', 'AceConsole-3.0', 'AceEvent-3.0', 'AceHook-3.0')
 _G[addonName] = AlomawaniQoL
 
-local LibDualSpec = LibStub('LibDualSpec-1.0', true)
-
 local defaults = {
-	profile = {
-	}
+    profile = {},
 }
 
-function AlomawaniQoL.Print(...)
-    _G.DEFAULT_CHAT_FRAME:AddMessage(strjoin('', '|cff00ff00', 'Alomawani QoL:|r ', ...))
+function AlomawaniQoL:Print(...)
+    DEFAULT_CHAT_FRAME:AddMessage(strjoin('', '|cff00ff00Alomawani QoL:|r ', ...))
 end
 
 function AlomawaniQoL:OnInitialize()
-	self.db = LibStub('AceDB-3.0'):New('AlomawaniQoLDB', defaults, true)
+    self.db = LibStub('AceDB-3.0'):New('AlomawaniQoLDB', defaults, true)
 
-	self.db.RegisterCallback(self, 'OnNewProfile', 'InitializeProfile')
-	self.db.RegisterCallback(self, 'OnProfileChanged', 'UpdateModuleConfigs')
-	self.db.RegisterCallback(self, 'OnProfileCopied', 'UpdateModuleConfigs')
-	self.db.RegisterCallback(self, 'OnProfileReset', 'UpdateModuleConfigs')
+    self.db.RegisterCallback(self, 'OnNewProfile', 'InitializeProfile')
+    self.db.RegisterCallback(self, 'OnProfileChanged', 'UpdateModuleConfigs')
+    self.db.RegisterCallback(self, 'OnProfileCopied', 'UpdateModuleConfigs')
+    self.db.RegisterCallback(self, 'OnProfileReset', 'UpdateModuleConfigs')
 
-	if LibDualSpec then
-		LibDualSpec:EnhanceDatabase(AlomawaniQoL.db, 'AlomawaniQoL')
-	end
+    if LibDualSpec then
+        LibDualSpec:EnhanceDatabase(self.db, 'AlomawaniQoL')
+    end
 
-	self:SetupOptions()
+    self:SetupOptions()
 end
 
 function AlomawaniQoL:InitializeProfile()
-	self:UpdateModuleConfigs()
+    self:UpdateModuleConfigs()
 end
 
 function AlomawaniQoL:OnEnable()
 end
 
 function AlomawaniQoL:UpdateModuleConfigs()
-	for _, v in self:IterateModules() do
-		v:ToggleModule()
-		if v:IsEnabled() and type(v.ApplyConfig) == 'function' then
-			v:ApplyConfig()
-		end
-	end
+    for _, module in self:IterateModules() do
+        module:ToggleModule()
+        if module:IsEnabled() and module.ApplyConfig then
+            module:ApplyConfig()
+        end
+    end
 end
 
-AlomawaniQoL.defaultModulePrototype = {}
-function AlomawaniQoL.defaultModulePrototype:ToggleModule(info, value)
-	if value ~= nil then
-		self.db.profile.enabled = value
-	else
-		value = self.db.profile.enabled
-	end
-	if value and not self:IsEnabled() then
-		self:Enable()
-	elseif not value and self:IsEnabled() then
-		self:Disable()
-	end
+local defaultModulePrototype = {}
+AlomawaniQoL.defaultModulePrototype = defaultModulePrototype
+
+function defaultModulePrototype:ToggleModule(info, value)
+    if value ~= nil then
+        self.db.profile.enabled = value
+    else
+        value = self.db.profile.enabled
+    end
+
+    if value and not self:IsEnabled() then
+        self:Enable()
+    elseif not value and self:IsEnabled() then
+        self:Disable()
+    end
 end
 
-function AlomawaniQoL.defaultModulePrototype:ToggleOptions()
-	if self.options then
-		self.options.args = self:IsEnabled() and self.modulesoptions or self.disabledoptions
-	end
+function defaultModulePrototype:ToggleOptions()
+    if self.options then
+        self.options.args = self:IsEnabled() and self.modulesoptions or self.disabledoptions
+    end
 end
 
-function AlomawaniQoL.defaultModulePrototype:OnDisable()
+function defaultModulePrototype:OnDisable()
     self:ToggleOptions()
 end
 
-AlomawaniQoL:SetDefaultModulePrototype(AlomawaniQoL.defaultModulePrototype)
+AlomawaniQoL:SetDefaultModulePrototype(defaultModulePrototype)
--- Modules/System/Graphics.lua
+++ Modules/System/Graphics.lua
@@ -1,51 +1,53 @@
 local _, AlomawaniQoL = ...
+
 local Graphics = AlomawaniQoL:NewModule('Graphics', 'AceEvent-3.0')
 
+local SetCVar = SetCVar
+
 local defaults = {
-	profile = {
+    profile = {
         enabled = true,
-		customScaleToggle = false,
-		customScaleValue = 0, -- 768/1440
-		maxCameraZoomToggle = false,
-		maxCameraZoomValue = 2.6
-	}
+        customScaleToggle = false,
+        customScaleValue = 0,
+        maxCameraZoomToggle = false,
+        maxCameraZoomValue = 2.6,
+    },
 }
 
 function Graphics:OnInitialize()
     self.db = AlomawaniQoL.db:RegisterNamespace('Graphics', defaults)
-
-	self:SetEnabledState(self.db.profile.enabled)
-
-	self:RegisterEvent('PLAYER_ENTERING_WORLD')
+    self:SetEnabledState(self.db.profile.enabled)
 end
 
 function Graphics:OnEnable()
     self:ToggleOptions()
-	self:ApplyConfig(self.db.profile)
+    self:RegisterEvent('PLAYER_ENTERING_WORLD')
+end
+
+function Graphics:OnDisable()
+    self:UnregisterEvent('PLAYER_ENTERING_WORLD')
+    self:ToggleOptions()
 end
 
 function Graphics:ApplyConfig(config)
-	if config then
-		self.config = config
-	end
+    if config then
+        self.config = config
+    end
 end
 
 function Graphics:PLAYER_ENTERING_WORLD()
+    local profile = self.db.profile
 
+    if profile.customScaleToggle and profile.customScaleValue ~= 0 then
+        UIParent:SetScale(tonumber(profile.customScaleValue) or profile.customScaleValue)
+    end
 
-	if self.db.profile.customScaleToggle then
-		if self.db.profile.customScaleValue ~= 0 then
-			-- TODO: Check the value type
-			UIParent:SetScale(self.db.profile.customScaleValue)
-		end
-	end
-
-	if self.db.profile.maxCameraZoomToggle then
-		maxCameraZoomValue = tonumber(self.db.profile.maxCameraZoomValue)
-		if maxCameraZoomValue < 1 or maxCameraZoomValue > 2.6 then
-			AlomawaniQoL.Print('The max camera zoom value must be between 0 and 2.6')
-		else
-			SetCVar('cameraDistanceMaxZoomFactor', maxCameraZoomValue)
-		end
-	end
+    if profile.maxCameraZoomToggle then
+        local zoomValue = tonumber(profile.maxCameraZoomValue)
+        if not zoomValue or zoomValue < 1 or zoomValue > 2.6 then
+            AlomawaniQoL:Print('The max camera zoom value must be between 1 and 2.6')
+        else
+            SetCVar('cameraDistanceMaxZoomFactor', zoomValue)
+        end
+    end
 end
