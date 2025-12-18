297c97497dcaf88b91a44cfda894dd01a36fb405
Lucas Lamboley <l.lamboley.mentor@aleo.agency>
2025-12-18 21:27:05 +0100

    :art: Optimize

--- Options/Options.lua
+++ Options/Options.lua
@@ -6,43 +6,39 @@ local AceConfig = LibStub('AceConfig-3.0')
 local LibDualSpec = LibStub('LibDualSpec-1.0', true)
 
 local function generateOptions()
-	local db = AlomawaniQoL.db
-
+    local db = AlomawaniQoL.db
     local profilesTable = AceDBOptions:GetOptionsTable(db)
 
     if LibDualSpec then
         LibDualSpec:EnhanceOptions(profilesTable, db)
     end
 
-	AlomawaniQoL.options = {
-		type = 'group',
-		name = 'Alomawani QoL',
-		childGroups = 'tree',
-		plugins = {},
-		args = {
-			system = {
-				order = 1,
-				type = 'group',
-				name = 'System',
-				args = {
-					options = {
-						type = 'group',
-						order = 0,
-						name = function(info)
-							return info.uiType == 'dialog' and '' or 'System'
-						end,
-						guiInline = true,
-						args = {
-						}
-					},
-				},
-			},
-		},
-	}
-
-	AlomawaniQoL.options.plugins.profiles = {
-		profiles = AceDBOptions:GetOptionsTable(AlomawaniQoL.db)
-	}
+    AlomawaniQoL.options = {
+        type = 'group',
+        name = 'Alomawani QoL',
+        childGroups = 'tree',
+        plugins = {
+            profiles = { profiles = profilesTable },
+        },
+        args = {
+            system = {
+                order = 1,
+                type = 'group',
+                name = 'System',
+                args = {
+                    options = {
+                        type = 'group',
+                        order = 0,
+                        name = function(info)
+                            return info.uiType == 'dialog' and '' or 'System'
+                        end,
+                        guiInline = true,
+                        args = {},
+                    },
+                },
+            },
+        },
+    }
 
     for _, module in AlomawaniQoL:IterateModules() do
         if module.SetupOptions then
@@ -52,27 +48,23 @@ local function generateOptions()
 end
 
 local function getOptions()
-	if not AlomawaniQoL.options then
-		generateOptions()
-		generateOptions = nil
-	end
-	return AlomawaniQoL.options
+    if not AlomawaniQoL.options then
+        generateOptions()
+    end
+    return AlomawaniQoL.options
 end
 
-function AlomawaniQoL:ToggleOptions(input)
-	if InCombatLockdown() then
-		AlomawaniQoL.Print("Can't open options in combat.")
-		return
-	end
-
-	AceConfigDialog:Open('AlomawaniQoL')
+function AlomawaniQoL:ToggleOptions()
+    if InCombatLockdown() then
+        self:Print("Can't open options in combat.")
+        return
+    end
+    AceConfigDialog:Open('AlomawaniQoL')
 end
 
 function AlomawaniQoL:SetupOptions()
-	AceConfig:RegisterOptionsTable('AlomawaniQoL', getOptions)
-
-	AceConfigDialog:SetDefaultSize('AlomawaniQoL', 660, 650)
-
+    AceConfig:RegisterOptionsTable('AlomawaniQoL', getOptions)
+    AceConfigDialog:SetDefaultSize('AlomawaniQoL', 660, 650)
     self:RegisterChatCommand('alomawaniqol', 'ToggleOptions')
     self:RegisterChatCommand('alq', 'ToggleOptions')
 end
--- Options/OptionsGraphics.lua
+++ Options/OptionsGraphics.lua
@@ -1,92 +1,81 @@
 local _, AlomawaniQoL = ...
+
 local Graphics = AlomawaniQoL:GetModule('Graphics')
 
-local myGetterFunc, mySetterFunc
-do
-	function myGetterFunc(info)
-		return Graphics.db.profile[info[#info]]
-	end
+local function myGetterFunc(info)
+    return Graphics.db.profile[info[#info]]
+end
 
-	function mySetterFunc(info, value)
-		Graphics.db.profile[info[#info]] = value
-	end
+local function mySetterFunc(info, value)
+    Graphics.db.profile[info[#info]] = value
 end
 
 function Graphics:SetupOptions()
-	if not self.options then
-		local enabled = {
-			type = 'toggle',
-			order = 1,
-			name = 'Enabled',
-			desc = 'Graphics',
-			get = function() return self.db.profile.enabled end,
-			set = 'ToggleModule',
-			handler = self,
-			width = 'full',
-		}
+    if not self.modulesoptions then
+        local enabled = {
+            type = 'toggle',
+            order = 1,
+            name = 'Enabled',
+            desc = 'Graphics',
+            get = function() return self.db.profile.enabled end,
+            set = 'ToggleModule',
+            handler = self,
+            width = 'full',
+        }
 
-		self.modulesoptions = {
-			enabled = enabled,
-			header1 = {
-				order = 2,
-				type = 'header',
-				name = 'General',
-				width = 'full',
-			},
-			customScaleToggle = {
-				order = 3,
-				type = 'toggle',
-				name = 'Use custom scale',
-				set = mySetterFunc,
-				get = myGetterFunc,
-			},
-			customScaleValue = {
-				order = 4,
-				type = 'input',
-				name = 'Custom scale value',
-				get = myGetterFunc,
-				set = mySetterFunc,
-				dialogControl = 'EditBox',
-			},
-			maxCameraZoomToggle = {
-				order = 5,
-				type = 'toggle',
-				name = 'Max camera zoom',
-				set = mySetterFunc,
-				get = myGetterFunc,
-			},
-			maxCameraZoomValue = {
-				order = 6,
-				name = 'Max camera zoom value',
-				desc = 'Configure the max camera zoom value. Default: 2.6',
-				type = 'input',
-				get = myGetterFunc,
-				set = mySetterFunc,
-				dialogControl = 'EditBox',
-			},
-			-- cameraZoomValue = {
-			-- 	order = 6,
-			-- 	name = 'Zoom value',
-			-- 	desc = 'Configure the max camera zoom value',
-			-- 	type = 'range',
-			-- 	min = 0.1, max = 3, softMin = 0.1, softMax = 2.6, bigStep = 0.1,
-			-- 	get = myGetterFunc,
-			-- 	set = mySetterFunc,
-			-- },
-		}
+        self.modulesoptions = {
+            enabled = enabled,
+            header1 = {
+                order = 2,
+                type = 'header',
+                name = 'General',
+                width = 'full',
+            },
+            customScaleToggle = {
+                order = 3,
+                type = 'toggle',
+                name = 'Use custom scale',
+                set = mySetterFunc,
+                get = myGetterFunc,
+            },
+            customScaleValue = {
+                order = 4,
+                type = 'input',
+                name = 'Custom scale value',
+                get = myGetterFunc,
+                set = mySetterFunc,
+            },
+            maxCameraZoomToggle = {
+                order = 5,
+                type = 'toggle',
+                name = 'Max camera zoom',
+                set = mySetterFunc,
+                get = myGetterFunc,
+            },
+            maxCameraZoomValue = {
+                order = 6,
+                name = 'Max camera zoom value',
+                desc = 'Configure the max camera zoom value. Default: 2.6',
+                type = 'input',
+                get = myGetterFunc,
+                set = mySetterFunc,
+            },
+        }
 
-		self.disabledoptions = {
-			enabled = enabled,
-		}
-	end
+        self.disabledoptions = {
+            enabled = enabled,
+        }
+    end
 
-	self.options = {
-		order = 1,
-		type = 'group',
-		name = 'Graphics',
-        childGroups = 'tab',
-	}
+    if not self.options then
+        self.options = {
+            order = 1,
+            type = 'group',
+            name = 'Graphics',
+            childGroups = 'tab',
+        }
+    end
 
-	AlomawaniQoL.options.args.system.args.Graphics = self.options
-	self.options.args = self:IsEnabled() and self.modulesoptions or self.disabledoptions
+    AlomawaniQoL.options.args.system.args.Graphics = self.options
+    self.options.args = self:IsEnabled() and self.modulesoptions or self.disabledoptions
 end
