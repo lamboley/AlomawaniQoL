018e490269741fc4d408e504e5568a58a82b30d8
Lucas Lamboley <l.lamboley.mentor@aleo.agency>
2026-01-25 14:56:05 +0100

    Add Details Library

--- Modules/Interface/Interface.lua
+++ Modules/Interface/Interface.lua
@@ -2,7 +2,36 @@ local _, addon = ...
 local T, C, L = addon[1], addon[2], addon[3]
 local Interface = T['Interface']
 
+local lastRightClick = 0
+
+local function DisableRightClick()
+    local time = GetTime()
+    if lastRightClick + 0.2 < time then
+        lastRightClick = time
+        MouselookStop()
+    end
+end
+
+function Interface:OnEvent(event, ...)
+    SetCVar('floatingCombatTextCombatHealing', 0)
+    SetCVar('floatingCombatTextCombatDamage', 0)
+    SetCVar('floatingCombatTextCombatLogPeriodicSpells', 0)
+    SetCVar('floatingCombatTextPetMeleeDamage', 0)
+    SetCVar('floatingCombatTextPetSpellDamage', 0)
+
+    WorldFrame:HookScript('OnMouseUp', function(_, button)
+        if button == "RightButton" and not IsMouseButtonDown('RightButton') then
+            if InCombatLockdown() then
+                DisableRightClick()
+            end
+        end
+    end)
+end
+
 function Interface:Enable()
+    self:RegisterEvent('PLAYER_ENTERING_WORLD')
+	self:SetScript('OnEvent', self.OnEvent)
+
     self.Quests:Enable()
     self.Social:Enable()
 end
